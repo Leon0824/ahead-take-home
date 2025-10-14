427f0e2f67b38fb7036f4d62fbcc33853f82c348
Leon <leon0824@gmail.com>
2025-10-14 20:37:09 +0800

    chore: Add app settings module

---
+++ app/settings.py
@@ -0,0 +1,56 @@
+from typing import ClassVar, Literal
+
+from pydantic import UUID4, AnyUrl, EmailStr, Field, FilePath, HttpUrl, RedisDsn, SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+
+class Settings(BaseSettings):
+    ENVIRONMENT_MODE: Literal[
+        'DEVELOPMENT', # 本機開發環境
+        'STAGING', # 測試站
+        'PRODUCTION', # 正式站
+        'TEST', # CI 測試環境
+    ]
+
+    BASE_URL: str
+    ALLOW_ORIGINS: list[str]
+
+    REDIS_URL: RedisDsn
+
+    ADMIN_EMAIL: EmailStr
+    ADMIN_PASSWORD: SecretStr
+
+    JWT_KEY: SecretStr
+
+    AWS_ACCESS_KEY_ID: str
+    AWS_SECRET_ACCESS_KEY: SecretStr
+    AWS_DEFAULT_REGION: str
+
+    _example: ClassVar[dict] = {
+        'ENVIRONMENT_MODE': 'DEVELOPMENT',
+
+        'BASE_URL': 'https://api.wnc.net',
+        'ALLOW_ORIGINS' : ['http://localhost:5173'],
+
+        'REDIS_URL': 'redis://redis-15500.c290.ap-northeast-1-2.ec2.redns.redis-cloud.com:15500',
+
+        'ADMIN_USERNAME': 'admin',
+        'ADMIN_PASSWORD': '**********',
+
+        'JWT_KEY': '**********',
+
+        'AWS_ACCESS_KEY_ID': 'AWSID',
+        'AWS_SECRET_ACCESS_KEY': 'AWSSECRET',
+        'AWS_DEFAULT_REGION': 'ap-northeast-1',
+    }
+    model_config = SettingsConfigDict(
+        json_schema_extra={'examples': [_example]},
+        extra='allow',
+        env_file='.env',
+        secrets_dir='/run/secrets',
+    )
+
+
+
+def get_settings(): return Settings()
