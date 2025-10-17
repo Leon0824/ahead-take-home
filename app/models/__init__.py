294cef59d1c6623a6730a26b2f9dfda7103663f1
Leon <leon0824@gmail.com>
2025-10-17 23:33:20 +0800

    feat: Upload FCS files to S3
    feat: Upload FCS files to S3
    
    feat: Upload FCS files to S3

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,15 +5,19 @@ from pydantic import BaseModel, ConfigDict
 from app.db import FcsFile
 
 
-class UploadBatchRead(BaseModel):
+class UploadBatchResult(BaseModel):
     batch_idno: str
     upload_time: datetime
     files: list[FcsFile]
+    failed_files: list[dict] = []
 
-    model_config = ConfigDict(json_schema_extra={
-        'examples': [{
-            "batch_idno": "01K7PXGBTMV8R5M3TZTJ79PSMF",
-            'upload_time': "2025-10-16T18:00:00Z",
-            "files": [],
-        }],
-    })
+    model_config = ConfigDict(
+        json_schema_extra={
+            'examples': [{
+                "batch_idno": "01K7PXGBTMV8R5M3TZTJ79PSMF",
+                'upload_time': "2025-10-16T18:00:00Z",
+                "files": [],
+                "failed_files": [],
+            }],
+        }
+    )
