56df2683ed37eeb0253a632c4025edaa851ccd2d
Leon <leon0824@gmail.com>
2025-10-20 09:25:09 +0800

    feat: Download FCS files from S3

--- job-service/jobs/settings.py
+++ job-service/jobs/settings.py
@@ -16,12 +16,19 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     REDIS_URL: RedisDsn
 
+    AWS_ACCESS_KEY_ID: str
+    AWS_SECRET_ACCESS_KEY: SecretStr
+    AWS_DEFAULT_REGION: str
+
     _example: ClassVar[dict] = {
         'ENVIRONMENT_MODE': 'DEVELOPMENT',
 
         'DATABASE_URL': 'sqlite:///database.db',
         'REDIS_URL': 'redis://redis-15500.c290.ap-northeast-1-2.ec2.redns.redis-cloud.com:15500',
 
+        'AWS_ACCESS_KEY_ID': 'AWSID',
+        'AWS_SECRET_ACCESS_KEY': 'AWSSECRET',
+        'AWS_DEFAULT_REGION': 'ap-northeast-1',
     }
     model_config = SettingsConfigDict(
         json_schema_extra={'examples': [_example]},
